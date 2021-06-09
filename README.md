4d0683f7c8dd066e1b8561f7eea2603e6f0a4e61
Alexandre <alexou73@gmail.com>
2021-06-09 18:02:15 +0200

    feat(endProject): end assignment project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# assignment_alex_airportsearch
+# Assignment of Algolia-Alexandre Viretti
 
 ## Project setup
 ```
@@ -20,5 +20,36 @@ npm run build
 npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Project
+
+### Stack
+
+I've chosen Vue JS because i'm more efficient with this framework. I use SCSS that i find more pratical than CSS. And of course HTML.
+
+### Duration
+
+To be honest, i begin this morning to 9, and i finish this afternoon arround 5 PM. So i take more than 3/4 hours recommended.
+
+## Steps
+
+### First step
+
+I follow the differents point on the mail, i didn't really encounter issues on setup with Algolia, i choose the dataset of Airports. Because i love travel and i had lots idea to implement on app. But if we had to redo it i chosen a different data.
+
+### First step
+
+I setup my Vue project, i download dependencies that i needed.
+
+### Third step
+
+Read the doc of Algolia, and tried to grab most of it.
+
+### Fourth step
+
+Developpement.
+
+## Steps
+
+Problems encountered: There is not enough data on airport to implement as I wanted, that's why if I had to do it again, i'll choose an others datas.
+
+So i needed to make call api, that's what i wanted to do for pictures, so i install axios, i call my api pixabay with city in params but it never worked i lost lot of time. I asked on stackOverFlow but no answer. My porblem is with Axios i can't return a link jpg to bind on the src of picture. So i decided to create a new component Card to store each link in data but i did not know how to do because of ais-hits is not an array or something else i can iterate over.
---
+++ src/components/Search.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="aeroport">
+    <div v-if="airports.length">
+      <div v-for="airport in airports" :key="airport" class="pane">
+        {{ airport }}
+      </div>
+    </div>
+    <div v-else>
+      <div class="pane">E</div>
+      <div class="pane">D</div>
+      <div class="pane">I</div>
+      <div class="pane">M</div>
+      <div class="pane">B</div>
+      <div class="pane">U</div>
+      <div class="pane">R</div>
+      <div class="pane">G</div>
+      <div class="pane">H</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["airports"],
+  data() {
+    return {
+      aeroportsBase: ["New York", "Paris", "Miami", "Sydney"],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  background: #444;
+  display: flex;
+  justify-content: center;
+}
+.aeroport {
+  text-align: center;
+  padding: 2em 0;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+}
+.pane {
+  width: 1em;
+  height: 1em;
+  display: inline-block;
+  margin: 0 auto;
+  border-radius: 0.05em;
+  border: 0.01em solid #444;
+  position: relative;
+  background: #222;
+  text-align: center;
+  line-height: 1;
+  font-size: 80px;
+  color: #fff;
+  font-family: monospace;
+  box-shadow: 0px 0.02em 0 #ccc, 0px 0.05em 0 #000;
+  text-shadow: -1px -2px 2px rgba(0, 0, 0, 1);
+  z-index: 50;
+  @media only screen and (max-width: 768px) {
+    width: 0.9em;
+    height: 0.9em;
+    font-size: 35px;
+  }
+}
+
+/*.pane:before {
+  display: block;
+  width: 100%;
+  height: 0px;
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 0;
+  margin: 0 auto;
+  margin-left: -.05em;
+  border: .05em solid black;
+  z-index: -10;
+  
+}*/
+
+.pane:after {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  content: "";
+  border-top: 2px solid #000;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.3);
+  width: 100%;
+  height: 0px;
+  opacity: 0.8;
+  z-index: 10;
+  margin-top: -1px;
+}
+</style>
